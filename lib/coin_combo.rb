38b71a2fd217913da3e741710715cd6b7c3812b3
George Denkey & Austin Kincaid <student@epicodus.com>
2015-08-06 11:03:27 -0700

    add spec and method to return coin combination beyond one coin

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -3,19 +3,31 @@ class Float
     amount = self
     coin_hash = Hash.new()
     coin_hash = {0.01 => [ " penny", " pennies"],  0.05  => [" nickel", " nickels"]}
+    amount_return = ""
 
-
-
-    if amount < 0.05
-      penny_amount = amount.*(100).to_s.sub(".0","")
-      if amount == 0.01
-        return penny_amount.concat(coin_hash.fetch(0.01)[0])
-      else
-        return penny_amount.concat(coin_hash.fetch(0.01)[1])
+    while amount > 0
+      if amount < 0.05
+        penny_amount = amount.*(100).ceil.to_s.slice(0..0)
+        # binding.pry
+        if amount < 0.01 && amount > 0
+          amount_return.concat(penny_amount.concat(coin_hash.fetch(0.01)[0]))
+        else
+          amount_return.concat(penny_amount.concat(coin_hash.fetch(0.01)[1]))
+        end
+        amount = 0
+      elsif amount <= 0.10
+        nickel_amount = amount.*(100)./(5).to_s.slice(0..0)
+        if amount < 0.10
+          amount_return = nickel_amount.concat(coin_hash.fetch(0.05)[0])
+        else
+          amount_return = nickel_amount.concat(coin_hash.fetch(0.05)[1])
+        end
+        amount -= 0.05
+        if amount > 0
+          amount_return.concat(" and ")
+        end
       end
-    elsif amount == 0.05
-      nickel_amount = amount.*(100)./(5).to_s.sub(".0","")
-      return nickel_amount.concat(coin_hash.fetch(0.05)[0])
-     end
+    end
+    return amount_return
   end
 end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -16,4 +16,8 @@ describe('Float#coin_combo') do
     expect((0.05).coin_combo).to(eq("1 nickel"))
   end
 
+  it("takes 0.06 and returns 'one nickel and one penny' as the smallest number of coins to give as change") do
+    expect((0.06).coin_combo).to(eq("1 nickel and 1 penny"))
+  end
+
 end
