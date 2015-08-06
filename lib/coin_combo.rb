c8f0e88c966f9038786b13f68763b9ef8d409583
George Denkey & Austin Kincaid <student@epicodus.com>
2015-08-06 11:26:47 -0700

    adding new spec test, as well as dime extension to method

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -2,14 +2,14 @@ class Float
   define_method(:coin_combo) do
     amount = self
     coin_hash = Hash.new()
-    coin_hash = {0.01 => [ " penny", " pennies"],  0.05  => [" nickel", " nickels"]}
+    coin_hash = {0.01 => [ " penny", " pennies"],  0.05  => [" nickel", " nickels"], 0.10 => [" dime", " dimes"]}
     amount_return = ""
 
     while amount > 0
       if amount < 0.05
         penny_amount = amount.*(100).ceil.to_s.slice(0..0)
         # binding.pry
-        if amount < 0.01 && amount > 0
+        if amount <= 0.01 && amount > 0
           amount_return.concat(penny_amount.concat(coin_hash.fetch(0.01)[0]))
         else
           amount_return.concat(penny_amount.concat(coin_hash.fetch(0.01)[1]))
@@ -18,11 +18,22 @@ class Float
       elsif amount <= 0.10
         nickel_amount = amount.*(100)./(5).to_s.slice(0..0)
         if amount < 0.10
-          amount_return = nickel_amount.concat(coin_hash.fetch(0.05)[0])
+          amount_return.concat(nickel_amount.concat(coin_hash.fetch(0.05)[0]))
         else
-          amount_return = nickel_amount.concat(coin_hash.fetch(0.05)[1])
+          amount_return.concat(nickel_amount.concat(coin_hash.fetch(0.05)[1]))
         end
         amount -= 0.05
+        if amount > 0
+          amount_return.concat(" and ")
+        end
+      elsif amount <= 0.20
+        dime_amount = amount.*(100)./(10).to_s.slice(0..0)
+        if amount < 0.20
+          amount_return = dime_amount.concat(coin_hash.fetch(0.10)[0])
+        else
+          amount_return = dime_amount.concat(coin_hash.fetch(0.10)[1])
+        end
+        amount -= 0.10
         if amount > 0
           amount_return.concat(" and ")
         end
--- spec/coin_combo_spec.rb
+++ spec/coin_combo_spec.rb
@@ -16,8 +16,12 @@ describe('Float#coin_combo') do
     expect((0.05).coin_combo).to(eq("1 nickel"))
   end
 
-  it("takes 0.06 and returns 'one nickel and one penny' as the smallest number of coins to give as change") do
+  it("takes 0.06 and returns '1 nickel and 1 penny' as the smallest number of coins to give as change") do
     expect((0.06).coin_combo).to(eq("1 nickel and 1 penny"))
+  end
+
+  it("takes 0.11 and returns '1 dime and 1 penny' as the smallest number of coins to give as change") do
+    expect((0.11).coin_combo).to(eq("1 dime and 1 penny"))
   end
 
 end
